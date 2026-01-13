5c41fe110e30ee021468a285d9fd93123357cbd7
GuzmanDsl <emmanuel.guzman@apsva.us>
2026-01-13 01:50:23 -0500

    click updated

--- chores/chore.js
+++ chores/chore.js
@@ -1,7 +1,9 @@
 submit_btm = document.getElementById("submitbtn")
 form = document.getElementById('chore-form')
 
-submit_btm.addEventListener("click", (e)=>{
+submit_btm.addEventListener("click", function(){
+    console.log(e)
+    console.log("submit pressed")
     e.preventDefault()
 
     form.requestSumbit()
