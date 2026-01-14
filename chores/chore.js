d91ee30e9e3c5c93b4a7038bf0d2e4968c1cfe8e
GuzmanDsl <emmanuel.guzman@apsva.us>
2026-01-14 11:04:48 -0500

    Updated submit button style

--- chores/chore.js
+++ chores/chore.js
@@ -1,11 +1,36 @@
 submit_btm = document.getElementById("submitbtn")
 form = document.getElementById('chore-form')
 
-submit_btm.addEventListener("click", function(){
-    console.log(e)
-    console.log("submit pressed")
-    e.preventDefault()
 
-    form.requestSumbit()
-    
+submit_btm.addEventListener("click", function () {
+
+    console.log("submit pressed")
+
+ 
+    // form.submit()
+
+    genMail()
+
 })
+
+
+function genMail() {
+
+    // Request values
+    const type = document.getElementById("type").value
+    const date = document.getElementById("date").value
+    const time = document.getElementById("time").value
+
+    email = ""
+
+    if (type === "dish") {
+        email = 
+        `You have a ${type} request.
+        Date: ${date}
+        Time: ${time} 
+        
+        Make sure to be ready and bring any necessary tools for the job!`
+    }
+
+    console.log(email)
+}
