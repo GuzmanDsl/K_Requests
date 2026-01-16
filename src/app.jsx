6a6aa23ddafc3761b98fe3875876ae2dc5f74aa7
GuzmanDsl <emmanuel.guzman@apsva.us>
2026-01-16 12:21:48 -0500

    Retry react

--- src/app.jsx
+++ src/app.jsx
@@ -4,22 +4,22 @@ import Home from "./pages/Home";
 import "./styles/theme.css";
 
 function ChoresPlaceholder() {
-  return (
-    <main style={{ padding: 30 }}>
-      <h2>Chores Page</h2>
-      <p>This will be your email request form page.</p>
-    </main>
-  );
+    return (
+        <main style={{ padding: 30 }}>
+            <h2>Chores Page</h2>
+            <p>This will be your email request form page.</p>
+        </main>
+    );
 }
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chores" element={<ChoresPlaceholder />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/chores" element={<ChoresPlaceholder />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
